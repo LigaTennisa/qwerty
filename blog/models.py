1ba157dbd427b011be552b26cc368d173eea6ba3
LigaTennisa <160637160+LigaTennisa@users.noreply.github.com>
2024-04-21 20:42:47 +0300

    Add files via upload

---
+++ blog/models.py
@@ -0,0 +1,62 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+import datetime
+
+
+class Post(models.Model):
+    COURT_CHOICES = (
+        ('ЛИДЕР',
+         'ЛИДЕР (Суходольский м-н, Восточный округ, Тюмень) '),
+        ('Sofi-land',
+         'Sofi-land (Лесобазовская 8а/1 Тура м-н, Ленинский округ, Тюмень) '),
+    )
+
+    court = models.CharField('Корт', max_length=20,
+                             choices=COURT_CHOICES, default='Court A')
+
+    training_date = models.DateField(
+        'Дата', default=datetime.date.today)
+    training_time = models.TimeField(
+        'Время', default=datetime.time(0, 0))
+    preferences = models.TextField('Пожелания', default='')
+
+    published_at = models.DateTimeField('Опубликовано', default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    responders = models.ManyToManyField(User, related_name='responded_posts')
+
+    def str(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-published_at']
+
+
+class Response(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Response by {self.author.username} to post {self.post.id}"
+
+
+class Responder(models.Model):
+    # Поля для класса Responder
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    # Другие поля и методы
+
+    def __str__(self):
+        return self.name
+
+
+class ExpectedGame(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Court(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
